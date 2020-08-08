c4f6d580b527ef2d5aadc9002ad98832735e1b97
Vasilev Sergey <sergey21va@mail.ru>
2020-08-08 21:31:43 +0300

    Add AOP dependencies

--- src/main/java/kurs/Application.java
+++ src/main/java/kurs/Application.java
@@ -3,8 +3,10 @@ package kurs;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Log
+@EnableAspectJAutoProxy
 @ComponentScan()
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/kurs/Aspects/Logger.java
@@ -0,0 +1,9 @@
+package kurs.Aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+public class Logger {
+
+}
